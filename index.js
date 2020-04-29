24fbc8a250b4ae63d04b1ea2bc129d5a394f80ee
Grasshopper <mikaela301@gmail.com>
2020-04-29 01:40:03 -0700

    updated files

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 //random # 1-6 for first dice
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-//dice1.png - dice6.png for leftdice
-var randomDiceImage1 = "images/dice" + randomNumber1 + ".png"; 
-
 //player 1 (left) dice image
-var player1 = document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1); 
+var player1 = document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png"); 
 
 //random # 1-6 for second dice 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-//dice1.png - dice6.png for right dice
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
-
 //player 2 (right) dice image
-var player2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2); 
+var player2 = document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png"); 
 
 //Displays winner
 if(randomNumber1 > randomNumber2) {
