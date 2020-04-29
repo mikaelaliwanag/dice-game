b22003483092ee860ed8d70ce4bb393ddb220317
Grasshopper <mikaela301@gmail.com>
2020-04-29 02:59:28 -0700

    added event listener to button. Gives random dice numbers and displays winner when clicked.

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-//random # 1-6 for first dice
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.getElementById("roll-button").addEventListener("click", diceRoll);
 
-//player 1 (left) dice image
-var player1 = document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNumber1 + ".png"); 
+function diceRoll() {
 
-//random # 1-6 for second dice 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+	//random # 1-6 for first dice
+	var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-//player 2 (right) dice image
-var player2 = document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png"); 
+	//player 1 (left) dice image
+	var player1 = document
+		.querySelectorAll("img")[0]
+		.setAttribute("src", "images/dice" + randomNumber1 + ".png");
 
-//Displays winner
-if(randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 1 wins! 🚩";
-} else if (randomNumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
-} else {
-    document.querySelector("h1").innerHTML = "Draw! 🤝";
+	//random # 1-6 for second dice
+	var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+	//player 2 (right) dice image
+	var player2 = document
+		.querySelectorAll("img")[1]
+		.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+	//Changes h1 to display winner
+
+	if (randomNumber1 > randomNumber2) {
+		document.querySelector("h1").innerHTML = "Player 1 wins! 🚩";
+	} else if (randomNumber2 > randomNumber1) {
+		document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
+	} else {
+		document.querySelector("h1").innerHTML = "Draw! 🤝";
+    }
+    
 }
 
