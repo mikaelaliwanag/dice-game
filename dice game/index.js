fa8f9097735576421aeeca06adff0c2a457bff5e
Grasshopper <mikaela301@gmail.com>
2020-04-29 00:49:40 -0700

    created js variables to randomize dice images for both dices when refreshed

--- dice game/index.js
+++ dice game/index.js
@@ -1,3 +1,18 @@
+//random # 1-6 for first dice
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
+//dice1.png - dice6.png for leftdice
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png"; 
+
+//player 1 (left) dice image
+var player1 = document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1); 
+
+//random # 1-6 for second dice 
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+//dice1.png - dice6.png for right dice
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+
+//player 2 (right) dice image
+var player2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2); 
 
